fe68fffc171cea59c85b8ce4d97689d9715f0e94
Mansour <theboom.kajas@gmail.com>
2024-02-19 11:26:44 +0100

    Horoscope API call with HttpUrlConnection

--- app/src/main/java/com/example/horoscope/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horoscope/activities/DetailActivity.kt
@@ -4,29 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.example.horoscope.R
+import com.example.horoscope.data.HoroscopeProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class DetailActivity : AppCompatActivity() {
 
+    var horoscopeName:String? = null
+
     lateinit var horoscopeTextView:TextView
+    lateinit var horoscopeLuckTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
         horoscopeTextView = findViewById(R.id.horoscopeTextView)
+        horoscopeLuckTextView = findViewById(R.id.horoscopeLuckTextView)
 
-        val name = intent.getStringExtra("HOROSCOPE_NAME")
+        horoscopeName = intent.getStringExtra("HOROSCOPE_NAME")
 
-        horoscopeTextView.text = name
+        horoscopeTextView.text = horoscopeName
+
+        getHoroscopeLuck()
     }
 
     private fun getHoroscopeLuck() {
         CoroutineScope(Dispatchers.IO).launch {
             // Llamada en segundo plano
+            val result = HoroscopeProvider().getHoroscopeLuck(horoscopeName!!)
             runOnUiThread {
                 // Modificar UI
+                horoscopeLuckTextView.text = result
             }
         }
     }
--- app/src/main/java/com/example/horoscope/data/HoroscopeProvider.kt
+++ app/src/main/java/com/example/horoscope/data/HoroscopeProvider.kt
@@ -1,45 +1,56 @@
 package com.example.horoscope.data
 
+import android.util.Log
+import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLConnection
 import javax.net.ssl.HttpsURLConnection
 
 class HoroscopeProvider {
-    suspend fun getHoroscopeLuck() {
-        val url = URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily") // URL de la API o endpoint
-        val requestBody = "sign=Aquarius&day=TODAY" // Datos a enviar en el cuerpo de la solicitud
+    suspend fun getHoroscopeLuck(horoscopeId: String): String? {
+        val url = URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=$horoscopeId&day=TODAY") // URL de la API o endpoint
+        var connection: HttpsURLConnection? = null
+        var result: String? = null
 
-        // Crear la conexión HTTP
-        val connection = url.openConnection() as HttpsURLConnection
-        connection.requestMethod = "POST" // Establecer el método POST
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded") // Establecer el tipo de contenido
+        try {
+            // Crear la conexión HTTP
+            connection = url.openConnection() as HttpsURLConnection
+            connection.requestMethod = "GET" // Establecer el método GET
+            connection.setRequestProperty("Accept", "application/json") // Establecer el tipo de contenido
 
-        // Habilitar el envío de datos en el cuerpo de la solicitud
-        connection.doOutput = true
-        val outputStream = connection.outputStream
-        outputStream.write(requestBody.toByteArray())
-        outputStream.flush()
-        outputStream.close()
+            // Leer la respuesta de la solicitud
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val response = readStream (connection.inputStream)
+                Log.i("HTTP", "Respuesta: ${response.toString()}")
 
-        // Leer la respuesta de la solicitud
-        val responseCode = connection.responseCode
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            val inputStream = connection.inputStream
-            val reader = BufferedReader(InputStreamReader(inputStream))
-            val response = StringBuilder()
-            var line: String?
-            while (reader.readLine().also { line = it } != null) {
-                response.append(line)
+                val responseObject: JSONObject = JSONObject(response.toString())
+                result = responseObject.getJSONObject("data").getString("horoscope_data")
+            } else {
+                Log.i("HTTP", "Error en la solicitud. Código de respuesta: $responseCode")
             }
-            reader.close()
-            println("Respuesta: ${response.toString()}")
-        } else {
-            println("Error en la solicitud. Código de respuesta: $responseCode")
-        }
 
-        // Cerrar la conexión
-        connection.disconnect()
+        } catch (e: Exception) {
+            Log.e("HTTP", "Error en la solicitud. ", e)
+        } finally {
+            // Cerrar la conexión
+            connection?.disconnect()
+        }
+        return result
+    }
+
+    private fun readStream (inputStream: InputStream) : StringBuilder {
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val response = StringBuilder()
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            response.append(line)
+        }
+        reader.close()
+        return response
     }
 }
